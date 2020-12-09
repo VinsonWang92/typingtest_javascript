fe882cced1f5f535ba16885b4c36721a2026605f
VinsonWang92 <vinsonwang92@gmail.com>
2020-12-10 09:42:26 +1100

    added timer

--- script.js
+++ script.js
@@ -4,15 +4,20 @@ const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
-var timer = 0;
+var timer = [0,0,0,0];
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 
 
 // Run a standard minute/second/hundredths timer:
 function runTimer() {
-    theTimer.innerHTML = timer;
-    timer++;
+    let currentTime = timer[0] + ":" + timer[1] + ":" + timer[2] + ":" + timer[3]
+    theTimer.innerHTML = currentTime;
+    timer[3]++;
+
+    timer[0] = Math.floor ((timer[3]/100)/60);
+    timer[1] = Math.floor ((timer[3]/100) - (timer[0]*60));
+    timer[2] = Math.floor (timer[3] - (timer[1]*100) - (timer[0]*6000));
 }
 
 
